59cb216342239b43bcb994ad58270414e9c4f20b
Daviaudh <daviaudh1@gmail.com>
2020-12-09 11:45:02 +0100

    conteurDeLettres

--- examPython.py
+++ examPython.py
@@ -9,9 +9,19 @@ tableauMot = ["WEBCAM", "ABOLIR","PIMENT","SAFARI","TUYAUX", "GARANT", "NACRES",
 numero = randrange(len(tableauMot))
 print(numero)
 print(tableauMot[numero])
+mot = tableauMot[numero]
+propositionMot = input('essayez de trouver le mot ')
+propositionMot = propositionMot.upper()
+lettre = 0
+for i in range (0,6):
+    for x in range (0,6) :
+        if mot[i] == propositionMot[x] :
+            
+            lettre=lettre + 1
+            
+        else:
+                lettre = lettre
+                
+print('il y a', lettre ,'lettre correspondante' )
 
-proposition = input('essayez de trouver le mot ')
-
-while proposition != tableauMot[numero]:
-    print('vous avez raté essayez encore ')
-    
+input()
