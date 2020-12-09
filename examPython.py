f50cc160e59218acee694abd43e1cf2b4e346bdc
Daviaudh <daviaudh1@gmail.com>
2020-12-09 12:42:41 +0100

    finPresqueJouable
    Aléatoires entre mots prédéfini dans une liste, lettres présentes dans le mot mise en avant avec colorama. affichage nombre lettres présentes mais redondance.

--- examPython.py
+++ examPython.py
@@ -1,7 +1,7 @@
 from colorama import init
 init()
 from colorama import Fore, Back, Style
-
+init(autoreset=True)
 from random import randrange
 
 
@@ -13,15 +13,26 @@ mot = tableauMot[numero]
 propositionMot = input('essayez de trouver le mot ')
 propositionMot = propositionMot.upper()
 lettre = 0
-for i in range (0,6):
+tableauLettreBonnePlace = ["" ,"" ,"" , "", "","" ]
+
+
+for i in range (0,6): 
     for x in range (0,6) :
-        if mot[i] == propositionMot[x] :
-            
+        if mot[i] == propositionMot[x] : #parcours les chaînes de caractères mot et proposition
             lettre=lettre + 1
-            
+            if i == x : 
+                tableauLettreBonnePlace[x] = 1 #crée un tableau avec les emplacements des lettres à la bonne place identifiées par un 1
+            else :
+                    tableauLettreBonnePlace[x] = 0
         else:
                 lettre = lettre
-                
-print('il y a', lettre ,'lettre correspondante' )
 
+for i in range (0,6): 
+    if tableauLettreBonnePlace[i] == 1: 
+        print(Back.RED + propositionMot[i], 'est à la bonne place') #si la lettres est au bonne endroit, elle est affichée en rouge
+           
+    
+
+print('il y a', lettre ,'lettre correspondante' )
+print(propositionMot)
 input()
